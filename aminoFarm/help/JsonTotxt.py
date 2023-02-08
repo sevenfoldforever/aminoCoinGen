c4e9044cc537dd21dec0d02d29e172ef29199941
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2023-02-08 22:28:31 +0300

    Delete aminoFarm directory

--- aminoFarm/help/JsonTotxt.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open(input("File name >>")+".json", "r") as file:
-    accounts: list = json.load(file)
-
-accounts = [f'{account["email"]} {account["password"]} {account["device"]}' for account in accounts]
-
-with open("accounts.txt", "w") as file:
-    file.write("\n".join(accounts))
